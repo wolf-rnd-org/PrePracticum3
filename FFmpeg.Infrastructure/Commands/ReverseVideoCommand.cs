dbf6685f9688871071914d6fe75136c9824b78fa
shacharracheli <r0583237001@gmail.com>
2025-07-23 13:01:11 +0300

    Reverse video-model & command

---
+++ FFmpeg.Infrastructure/Commands/ReverseVideoCommand.cs
@@ -0,0 +1,31 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class ReverseVideoCommand:BaseCommand,ICommand<ReverseVideoModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public WatermarkCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+        public async Task<CommandResult> ExecuteAsync(ReverseVideoModel model)
+        {
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddOption($"-vf reverse")
+                .SetInput(model.OutputFile);
+        
+
+            return await RunAsync();
+        }
+    }
+}
