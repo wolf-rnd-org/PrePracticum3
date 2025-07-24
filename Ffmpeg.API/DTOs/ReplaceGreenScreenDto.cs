262359c780c570155b2b006d0210011f9b1b5bdf
r7055 <ruchami7055@gamil.com>
2025-07-24 18:03:37 +0300

    create RepalceGreenScreen model and command

---
+++ Ffmpeg.API/DTOs/ReplaceGreenScreenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FFmpeg.API.DTOs
+{
+    public class ReplaceGreenScreenDto
+    {
+        public IFormFile VideoFile { get; set; }
+        public IFormFile GreenScreenFile { get; set; }
+        public string OutputVideoName { get; set; } = "output.mp4";
+    }
+}
