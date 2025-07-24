f15cdcffa803bb9c32987ab2d5ca852bef3b6dd1
ruth <r0556763949@gmail.com>
2025-07-25 00:18:20 +0300

    timestamp-command initial

---
+++ FFmpeg.Infrastructure/Commands/TimestampCommand.cs
@@ -0,0 +1,34 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+        public class TimestampCommand : BaseCommand, ICommand<TimestampModel>
+        {
+            private readonly ICommandBuilder _commandBuilder;
+
+            public TimestampCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+                : base(executor)
+            {
+                _commandBuilder = commandBuilder;
+            }
+
+            public async Task<CommandResult> ExecuteAsync(TimestampModel model)
+            {
+                string drawtextFilter = $"drawtext=text='%{{pts\\:hms}}':x={model.X}:y={model.Y}:fontsize={model.FontSize}:fontcolor={model.FontColor}";
+
+                CommandBuilder = _commandBuilder
+                    .SetInput(model.InputFile)
+                    .AddFilterComplex(drawtextFilter)
+                    .SetOutput(model.OutputFile);
+
+                return await RunAsync();
+            }
+        }
+    }
--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -14,6 +14,8 @@ namespace FFmpeg.Infrastructure.Services
     public interface IFFmpegServiceFactory
     {
         ICommand<WatermarkModel> CreateWatermarkCommand();
+        ICommand<TimestampModel> CreateTimestampCommand();
+
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -36,5 +38,10 @@ namespace FFmpeg.Infrastructure.Services
         {
             return new WatermarkCommand(_executor, _commandBuilder);
         }
+        public ICommand<TimestampModel> CreateTimestampCommand()
+        {
+            return new TimestampCommand(_executor, _commandBuilder);
+        }
+
     }
 }
