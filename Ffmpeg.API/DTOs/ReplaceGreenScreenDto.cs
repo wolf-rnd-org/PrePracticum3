eca4558f7d227d369e547c6267de2dc73441cdb1
r7055 <ruchami7055@gamil.com>
2025-07-24 18:48:12 +0300

    finish command

--- Ffmpeg.API/DTOs/ReplaceGreenScreenDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FFmpeg.API.DTOs
-{
-    public class ReplaceGreenScreenDto
-    {
-        public IFormFile VideoFile { get; set; }
-        public IFormFile GreenScreenFile { get; set; }
-        public string OutputVideoName { get; set; } = "output.mp4";
-    }
-}
