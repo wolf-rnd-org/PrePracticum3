35b1f1c66dacb926120001f438a1aec4e294948f
ruth <r0556763949@gmail.com>
2025-07-27 17:46:05 +0300

    add model

--- FFmpeg.Core/Models/TimestampModel.cs
+++ FFmpeg.Core/Models/TimestampModel.cs
@@ -8,6 +8,11 @@ namespace FFmpeg.Core.Models
 {
     public class TimestampModel
     {
-
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public int X { get; set; } = 10;
+        public int Y { get; set; } = 10;
+        public int FontSize { get; set; } = 24;
+        public string FontColor { get; set; } = "white";
     }
 }
