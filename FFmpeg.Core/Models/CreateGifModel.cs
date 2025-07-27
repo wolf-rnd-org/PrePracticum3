78ef752c8eea36f8901a1ecc988639c77c81a985
malka9791 <m0527169791@gmail.com>
2025-07-27 16:31:12 +0300

    add Command and Model of createGif

---
+++ FFmpeg.Core/Models/CreateGifModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Core.Models
+{
+    public class CreateGifModel
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public int Fps { get; set; } = 10;
+        public int YPosition { get; set; }
+        public int Width { get; set; } = 320;
+
+    }
+}
---
+++ FFmpeg.Infrastructure/Commands/CreateGifCommand.cs
@@ -0,0 +1,30 @@
+﻿using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class CreateGifCommand:BaseCommand,ICommand<CreateGifModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public WatermarkCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(CreateGifModel model)
+        {
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddOption("-vf", $"fps={model.Fps},scale={model.Width}:-1")
+                .SetOutput(model.OutputFile, true);
+
+            return await RunAsync();
+        }
+    }
+}
