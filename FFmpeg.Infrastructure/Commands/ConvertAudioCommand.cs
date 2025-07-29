6d074253cf6041a0ceaf838d4eaf461f76e6a5eb
RachelMantel <rrm85040@gmail.com>
2025-07-29 15:53:50 +0300

    save changes after pull finall

--- FFmpeg.Infrastructure/Commands/ConvertAudioCommand.cs
+++ FFmpeg.Infrastructure/Commands/ConvertAudioCommand.cs
@@ -7,19 +7,16 @@ namespace Ffmpeg.Command.Commands
     public class ConvertAudioCommand : BaseCommand, ICommand<ConvertAudioModel>
     {
         private readonly ICommandBuilder _builder;
-
         public ConvertAudioCommand(FFmpegExecutor executor, ICommandBuilder builder)
             : base(executor)
         {
             _builder = builder;
         }
-
         public async Task<CommandResult> ExecuteAsync(ConvertAudioModel model)
         {
             CommandBuilder = _builder
                 .SetInput(model.InputFile)
                 .SetOutput(model.OutputFile);
-
             return await RunAsync();
         }
     }
